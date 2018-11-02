6724ab1db170ba9055790796938bfc98d74c85b0
dnljsn <daniel@kalood.com>
2018-11-02 09:52:51 -0600

    React Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './component/Dashboard/Dashboard';
+import Wizard from './component/Wizard/Wizard';
+import Header from './component/Header/Header';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Dashboard />
+        <Wizard />
+        <Header />
       </div>
     );
   }
---
+++ src/component/Wizard/Wizard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Wizard extends Component {
+    render() {
+        return (
+            <div>
+                Wizard
+            </div>
+        )
+    }
+}
+
+export default Wizard;
